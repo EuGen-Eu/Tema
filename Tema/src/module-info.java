d8b5ad5e78ebd62e2fc7d2007a8b0a35bacf9ebb
EuGen-Eu <eugen.panait6000@gmail.com>
2025-06-02 12:03:23 +0300

    Update module-info.java

--- Tema/src/module-info.java
+++ Tema/src/module-info.java
@@ -3,7 +3,9 @@ public class CalculSimplu {
         int a = 10;
         int b = 20;
         int suma = a + b;
+        int diferenta = a - b;
 
         System.out.println("Rezultatul adunării este: " + suma);
+        System.out.println("Diferenta: " + diferenta);
     }
 }
