605fe41e621107eea30c04cfc184342be8840b82
Veve <Veve@Vera>
2025-06-02 12:00:40 +0300

    primul commit

--- Tema/src/module-info.java
+++ Tema/src/module-info.java
@@ -1,8 +1,9 @@
-/**
- * 
- */
-/**
- * 
- */
-module Tema {
+public class CalculSimplu {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int suma = a + b;
+
+        System.out.println("Rezultatul adunării este: " + suma);
+    }
 }
